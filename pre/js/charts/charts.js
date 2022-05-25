2ff9489ac39cb19a5a798825d288e1020d67164f
Carlos Muñoz Díaz <47306703G@cchs.csic.es>
2022-05-25 09:45:30 +0200

    Cambio de repo

--- pre/js/charts/charts.js
+++ pre/js/charts/charts.js
@@ -25,7 +25,7 @@ export function initChart() {
     //Creación de otros elementos relativos al gráfico que no requieran lectura previa de datos > Selectores múltiples o simples, timelines, etc 
 
     //Lectura de datos
-    d3.csv('https://raw.githubusercontent.com/CarlosMunozDiazCSIC/informe_perfil_mayores_2022_demografia_1_2/main/data/piramide_2021_urbano_rural.csv', function(error,data) {
+    d3.csv('https://raw.githubusercontent.com/EnvejecimientoEnRed/informe_perfil_mayores_2022_demografia_1_2/main/data/piramide_2021_urbano_rural.csv', function(error,data) {
         if (error) throw error;
 
         //SELECCIÓN DE ELEMENTOS
